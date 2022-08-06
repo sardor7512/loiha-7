20e1d15bd214eb1888da343a5e237dde0dfcb3c2
sardor7512 <110720562+sardor7512@users.noreply.github.com>
2022-08-06 15:02:14 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+gsap.to("#img",{
+    scrollTrigger :{
+        scrub:0.5,
+    },
+    x:1500,
+    y:500,
+})
+
+
+
+
+
+gsap.from("#wolf",{
+    scrollTrigger :{
+        scrub:0.5,
+    },
+    x:400,
+    
+})
+
+
+
+gsap.from("#castle",{
+    scrollTrigger :{
+        scrub:0.5,
+    },
+    x:-80,
+    
+})
+
+
+
+gsap.to("#bats",{
+    scrollTrigger :{
+        scrub:0.5,
+    },
+    scale:1.5
+    
+})
